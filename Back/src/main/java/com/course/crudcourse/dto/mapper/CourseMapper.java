fcb3b807aaf34e0c40b08b4884770a86a7f1c642
Matheus-AMoreira <matheus.moreira2172@gmail.com>
2024-07-03 21:24:16 -0300

    create and update lessons

--- Back/src/main/java/com/course/crudcourse/dto/mapper/CourseMapper.java
+++ Back/src/main/java/com/course/crudcourse/dto/mapper/CourseMapper.java
@@ -4,6 +4,7 @@ import com.course.crudcourse.dto.CourseDTO;
 import com.course.crudcourse.dto.LessonDTO;
 import com.course.crudcourse.enums.Category;
 import com.course.crudcourse.model.Course;
+import com.course.crudcourse.model.Lesson;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -34,6 +35,17 @@ public class CourseMapper {
         }
         course.setName(courseDTO.name());
         course.setCategory(convertCategoryValue(courseDTO.category()));
+
+        List<Lesson> lessons = courseDTO.lessons().stream().map(lessonDTO -> {
+            var lesson = new Lesson();
+            lesson.setId(lessonDTO.id());
+            lesson.setName(lessonDTO.name());
+            lesson.setYoutubeUrl(lessonDTO.youtubeUrl());
+            lesson.setCourse(course);
+            return lesson;
+        }).collect(Collectors.toList());
+        course.setLessons(lessons);
+
         return course;
     }
 
